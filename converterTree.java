820e019adcab5ce8d00923b3c1df10c5574298d2
Sam Wizon <svw223@nyu.edu>
2013-06-13 20:52:01 -0300

    Update converterTree.java

--- converterTree.java
+++ converterTree.java
@@ -1,3 +1,5 @@
+/*converterTree uses a stack in order to evaluate postfix notation*/
+
 package CSHomework.HW3;
 import java.util.Stack;
 import java.util.Scanner;
@@ -9,74 +11,93 @@ public class converterTree  {
   {
     int length = line.length();  
     String postFix = "";
-    BoundedStackInterface<String> stack = new ArrayStack<String>(length);  
+    BoundedStackInterface<String> stack = new ArrayStack<String>(length);	//initialize stack
     
     for(int i = 0; i < length; i++){
+    	
       char value = line.charAt(i);
       String s3 = Character.toString(value);
          
-         if(value  >= '0' & value<= '9')
-            {      
+         if(value  >= '0' & value<= '9'){
+         	     
                postFix = postFix + value;
-			   postFix = postFix + " ";
+	       postFix = postFix + " ";
             }        
-         else if("(".equals(s3))
-           {
+            
+         else if("(".equals(s3)){
+           
              String s1 = Character.toString(value);
              stack.push(s1);
            }
          
                   
            else if(")".equals(s3)){
+           	
              while(!("(".equals(stack.top()))){
+             	
                String x = stack.top();
                stack.pop();
                postFix = postFix + x;
                postFix = postFix + " ";
             
              }
+             
            }    
          else if(value=='+' || value=='-' || value=='*' || value=='/' || value=='^'){
            
              String s2 = Character.toString(value);
+             
              if(stack.isEmpty()){
-               stack.push(s2);}
+               stack.push(s2);
+              }
              
              else if(checkPrecedence(stack.top()) <= checkPrecedence(s2)){
-               stack.push(s2);}
+               stack.push(s2);
+             }
              
              else if(checkPrecedence(stack.top()) > checkPrecedence(s2)){
                  
                while(!(stack.isEmpty())){
+               	
                  if("(".equals(stack.top())){
+                 	
                    stack.pop();
-                   break;}
+                   break;
+                   
+                  }
+                  
                  if(checkPrecedence(stack.top()) > checkPrecedence(s2)){
+                 	
                    String r = stack.top();
                    stack.pop();
                    postFix = postFix + r;
                    postFix = postFix + " ";
-               }
+                   
+                   }
+                   
                break;
                  
              }
-               stack.push(s2);
+        stack.push(s2);
              }
-
          }
-
   }   
             while(!(stack.isEmpty())){
-      if("(".equals(stack.top())){
-              stack.pop();}
-      else{
-        String g = stack.top();
-      stack.pop();
-      postFix = postFix + g;
-      postFix = postFix + " ";
+            	
+      		if("(".equals(stack.top())){
+      			
+              		stack.pop();
+              	}
+              	
+      		else{
+      			
+        	String g = stack.top();
+      		stack.pop();
+      		postFix = postFix + g;
+      		postFix = postFix + " ";
 
-      }
-      }
+      		}
+      	}
             return postFix; 
   }
   
