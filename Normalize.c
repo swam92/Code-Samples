a8fe50813b42f79f6766212121e7ea3487fa7f94
Sam Wizon <svw223@nyu.edu>
2013-06-13 20:25:19 -0300

    Update Normalize.c

--- Normalize.c
+++ Normalize.c
@@ -6,8 +6,8 @@ int normalize(char *buf, /* The character array containing the string to be norm
 {
   //Print initial array	
   int j=0;
-  while(j<len)
-    {
+  while(j<len){
+    
       printf("%c", (char)buf[j]);
       j++;
     }
@@ -19,31 +19,31 @@ int normalize(char *buf, /* The character array containing the string to be norm
   int i;           							//array index
   int ctr=0;       							//number of deleted characters
   
-	for (i=0; i<len; i++){ 						//for each element in the array
+ for (i=0; i<len; i++){ 						//for each element in the array
 	
-             if(isspace(*read)!=0){					//if what read is pointing to is a space
+      if(isspace(*read)!=0){					        //if what read is pointing to is a space
           
-       		if(write == buf){					//if nothing has been written yet
+       	if(write == buf){					       //if nothing has been written yet
     			
-      			while(isspace(*read) != 0){		       //while read is still pointing to a blank
+      		while(isspace(*read) != 0){		               //while read is still pointing to a blank
         			
-          				read++;				//increment the location of read
-           				ctr++;				//increment the amount of unused characters
+          		read++;				               //increment the location of read
+           		ctr++;            			       //increment the amount of unused characters
         		}
       			*write =getLower((int) *read); 			//break out of loop and assign write its first character
           	}
 
-        	if (isspace(buf[i-1])!=0){				//if there are two consecutive spaces
+        if (isspace(buf[i-1])!=0){				        //if there are two consecutive spaces
          
-           		while (isspace(*read) != 0){			//while there is more than one consecutive space
+           	while (isspace(*read) != 0){			       //while there is more than one consecutive space
           
             		read++;						//increment read pointer
           	  	ctr++;					       //increment the amount of unused characters
-          		}
           	}
+          }
 
-          if(buf[i+1]==NULL){          					//if there is one extra trailing space
-        		ctr++;						//record it to be deleted
+       if(buf[i+1]==NULL){          					//if there is one extra trailing space
+        ctr++;						                //record it to be deleted
           }
        }
 
@@ -54,16 +54,16 @@ int normalize(char *buf, /* The character array containing the string to be norm
     }
   
    int temp = ctr;								
-   while(temp > 0)							//for each extra space at the end
-    {
+   while(temp > 0){							//for each extra space at the end
+    
      	read=NULL;							//assign each extra space null to delete them
      	temp--;
     }
 
   //print normalized array
   int k=0;
-  while(k<len)
-    {
+  while(k<len){
+    
       printf("%c", (char)buf[k]);
       k++;
     }
