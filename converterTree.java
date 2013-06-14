379efb9c6b82265eb7ca3cad8805e2a1a336d485
Sam Wizon <svw223@nyu.edu>
2013-06-13 21:06:47 -0300

    Update converterTree.java

--- converterTree.java
+++ converterTree.java
@@ -13,29 +13,29 @@ public class converterTree  {
     String postFix = "";
     BoundedStackInterface<String> stack = new ArrayStack<String>(length);	//initialize stack
     
-    for(int i = 0; i < length; i++){
+    for(int i = 0; i < length; i++){						//iterate through infix
     	
-      char value = line.charAt(i);
+      char value = line.charAt(i);						//tokenize
       String s3 = Character.toString(value);
          
-         if(value  >= '0' & value<= '9'){
+         if(value  >= '0' & value<= '9'){					//if the token is a number
          	     
-               postFix = postFix + value;
+               postFix = postFix + value;					//add it to postfix
 	       postFix = postFix + " ";
             }        
             
-         else if("(".equals(s3)){
+         else if("(".equals(s3)){						//if there is an open parenthesis
            
-             String s1 = Character.toString(value);
+             String s1 = Character.toString(value);				//push on to stack for future use
              stack.push(s1);
            }
          
                   
-           else if(")".equals(s3)){
+           else if(")".equals(s3)){						//if there is a closing parenthesis
            	
-             while(!("(".equals(stack.top()))){
+             while(!("(".equals(stack.top()))){					//until the open open parenthesis
              	
-               String x = stack.top();
+               String x = stack.top();						//append stack contents to postfix
                stack.pop();
                postFix = postFix + x;
                postFix = postFix + " ";
@@ -43,32 +43,33 @@ public class converterTree  {
              }
              
            }    
-         else if(value=='+' || value=='-' || value=='*' || value=='/' || value=='^'){
+           
+         else if(value=='+' || value=='-' || value=='*' || value=='/' || value=='^'){//if the token is an operator
            
              String s2 = Character.toString(value);
              
-             if(stack.isEmpty()){
-               stack.push(s2);
+             if(stack.isEmpty()){						     //if there is nothing on the stack
+               stack.push(s2);							     //immediately push operator
               }
              
-             else if(checkPrecedence(stack.top()) <= checkPrecedence(s2)){
-               stack.push(s2);
-             }
+             else if(checkPrecedence(stack.top()) <= checkPrecedence(s2)){	     //if the current token has higher
+               stack.push(s2);							     //precedence than the top of the
+             }									     //stack push it on the stack
              
-             else if(checkPrecedence(stack.top()) > checkPrecedence(s2)){
+             else if(checkPrecedence(stack.top()) > checkPrecedence(s2)){	     //if the current token has lower precdence
                  
-               while(!(stack.isEmpty())){
+               while(!(stack.isEmpty())){					     //while there are still tokens on stack
                	
-                 if("(".equals(stack.top())){
+                 if("(".equals(stack.top())){					     //stop popping after you reach an open parenthesis
                  	
                    stack.pop();
                    break;
                    
                   }
                   
-                 if(checkPrecedence(stack.top()) > checkPrecedence(s2)){
+                 if(checkPrecedence(stack.top()) > checkPrecedence(s2)){	     //if the top of stack has higher precedence
                  	
-                   String r = stack.top();
+                   String r = stack.top();					     //append token at top of stack to postfix
                    stack.pop();
                    postFix = postFix + r;
                    postFix = postFix + " ";
@@ -78,7 +79,7 @@ public class converterTree  {
                break;
                  
              }
-        stack.push(s2);
+        stack.push(s2);								   // after previous 2 conditionals push the current token on stack
              }
          }
   }   
